78b9fa2110c92cf7e6b7be4cfdf6b4cb54b3e7ee
darklore <zodiac.brave.story@gmail.com>
2021-10-12 16:30:49 +0900

    add get command

--- pkg/kafka/connect/connector.go
+++ pkg/kafka/connect/connector.go
@@ -3,5 +3,17 @@ package connect
 type ConnectorName string
 
 type Connector struct {
-	name ConnectorName `json:"name"`
+	Name   ConnectorName   `json:"name"`
+	Config ConnectorConfig `json:"config"`
+	Type   string          `json:"type"`
+	Tasks  []Task          `json:"tasks"`
+}
+
+type ConnectorConfig struct {
+	ConnectorClass string `json:"connector.class"`
+}
+
+type Task struct {
+	Connector string `json:"connector"`
+	Task      int    `json:"task"`
 }
