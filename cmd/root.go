654139c78fef5b494c6465f8663d0b34f344ee09
darklore <zodiac.brave.story@gmail.com>
2021-10-18 13:19:20 +0900

    Remove unused modules

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,13 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "kafka-connect-cli",
 	Short: "Command line tool around kafka connect REST interface",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -25,9 +17,6 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kafka-connect-cli.yaml)")
 	rootCmd.PersistentFlags().StringP("endpoint", "e", "http://localhost:8083", "Kafka connect REST endpoint")
 }
 
@@ -37,28 +26,4 @@ func getEndpoint(cmd *cobra.Command) (string, error) {
 		return "", err
 	}
 	return endpoint, nil
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".kafka-connect-cli" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".kafka-connect-cli")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
