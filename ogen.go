d95c65719117e85799d920ff5f1c110392e100c3
darklore <958690+darklore@users.noreply.github.com>
2025-06-23 00:14:07 +0900

    upgrade

--- ogen.go
+++ ogen.go
@@ -1,3 +1,3 @@
 package ogen
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@v1.10.1 -package ogen -clean -target internal/ogen openapi/connect_rest.yaml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@v1.14.0 -package ogen -clean -target internal/ogen openapi/connect_rest.yaml
